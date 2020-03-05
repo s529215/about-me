85cf1c384baca0d6aaf06ea271d355f19971a5cf
s529215 <42754268+s529215@users.noreply.github.com>
2020-03-05 13:40:32 -0600

    adding event listener and script

--- javascript/convert.js
+++ javascript/convert.js
@@ -1,7 +1,6 @@
 function convert(){
     const miles = parseFloat(document.getElementById("miles").value);
     const feet = (miles * 5280);
-
     document.getElementById("feet").innerHTML = feet+'ft';
     return false;
 }
