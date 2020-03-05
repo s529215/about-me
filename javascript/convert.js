c305f5fa0a7504a0e9d05af9ddcfd92d1561da7e
s529215 <42754268+s529215@users.noreply.github.com>
2020-03-05 14:33:51 -0600

    local storage completed

--- javascript/convert.js
+++ javascript/convert.js
@@ -2,5 +2,14 @@ function convert(){
     const miles = parseFloat(document.getElementById("miles").value);
     const feet = (miles * 5280);
     document.getElementById("feet").innerHTML = feet+'ft';
-    return false;
+    if(localStorage.counter){
+        localStorage.counter = Number(localStorage.counter) + 1;
+    }
+    else {
+        localStorage.counter = 1;
+    }
+    document.getElementById("num").innerHTML= localStorage.counter;
+}
+function empty(){
+    window.localStorage.clear();
 }
