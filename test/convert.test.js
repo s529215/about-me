381c0c6a5d1e13f7af92c1d17227c0b78ad55e4d
s529215 <42754268+s529215@users.noreply.github.com>
2020-03-06 10:26:10 -0600

    fixing QUnit tests

--- test/convert.test.js
+++ test/convert.test.js
@@ -1,28 +1,28 @@
 function convertTest(miles){
-    retrun (miles * 5280)
+    return (miles * 5280)
 }
 
-QUnit.test("convert", function(assert){
-    const result = convertTest(1);
+QUnit.test("convert 1mi to feet", function(assert){
+    var result = convertTest(1);
     assert.equal(result, "5280", "Testing base case");
 })
 
-QUnit.test("convert", function(assert){
+QUnit.test("convert 10mi to feet", function(assert){
     const result = convertTest(10);
     assert.equal(result, "52800", "Testing double digits");
 })
 
-QUnit.test("convert", function(assert){
+QUnit.test("convert 100mi to feet", function(assert){
     const result = convertTest(100);
     assert.equal(result, "528000", "Testing triple digits");
 })
 
-QUnit.test("convert", function(assert){
+QUnit.test("convert 2.5mi to feet", function(assert){
     const result = convertTest(2.5);
     assert.equal(result, "13200", "Testing decimals");
 })
 
-QUnit.test("convert", function(assert){
+QUnit.test("convert -5mi to feet", function(assert){
     const result = convertTest(-5);
     assert.equal(result, "-26400", "Testing negative");
 })
